4702e435140b84922a0c6a7c6d3477fa1ab83100
Labtory-82 <chnny9@gmail.com>
2025-11-13 23:57:25 +0900

    asdf

--- backend/src/main/java/com/backend/api/answer/dto/request/AnswerCreateRequest.kt
+++ backend/src/main/java/com/backend/api/answer/dto/request/AnswerCreateRequest.kt
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.Size
 @JvmRecord
 data class AnswerCreateRequest(
     @field:NotBlank(message = "답변 내용을 입력해주세요.")
-    @field:NotNull(message = "답변 내용을 입력해주세요.")
     @field:Size(min = 1, max = 1000, message = "답변 내용은 1자 이상 1000자 이하여야 합니다.")
     @Schema(description = "면접 답변 내용", example = "이것은 면접 답변입니다.")
 //    @JsonProperty(required = true)
@@ -19,3 +18,4 @@ data class AnswerCreateRequest(
 //    @JsonProperty(required = true)
     val isPublic: Boolean
 )
+
